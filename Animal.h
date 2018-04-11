bcbe9f4d4953e5eeb45382cac97a08f41a83fb63
Romain Fontaine <romain.fontaine73@gmail.com>
2018-04-10 19:37:59 -0500

    Non-lineal transformations

--- Animal.h
+++ Animal.h
@@ -4,12 +4,16 @@
 class Animal {
 private:
     std::vector<Point> shape;
-    double scaleX, scaleY;
+    double scaleX, scaleY, perspectiveRatio;
 public:
 
-    Animal(const std::vector<Point> &shape, const double &scaleX = 1, const double &scaleY = 1) :
-    shape(shape), scaleX(scaleX), scaleY(scaleY) {
+    Animal(const std::vector<Point> &shape, const double &scaleX = 1, const double &scaleY = 1, const double &perspectiveRatio = 0) :
+    shape(shape), scaleX(scaleX), scaleY(scaleY), perspectiveRatio(perspectiveRatio) {
 
+    }
+
+    void setPerspective(const double &v) {
+        this->perspectiveRatio = v;
     }
 
     void setScaleX(const double &scaleX) {
@@ -21,11 +25,23 @@ public:
     }
 
     void Draw(double x = 0, double y = 0) {
+        x-=0.5*scaleX; // Center
         glColor3d(1., 1., 1.);
         glBegin(GL_LINE_LOOP);
-        for (const Point &p : shape)
-            glVertex3d(x + p.x * scaleX, y + p.y * scaleY, p.z);
+        for (const Point &p : shape) {
+            glVertex3d(x + p.x * scaleX, y + (p.y + sign(p.y)*(exp(p.x * perspectiveRatio) - 1)) * scaleY, p.z);
+
+
+        }
         glEnd();
+    }
+
+    double sign(double d) {
+        if (d > .05)
+            return 1;
+        if (d<-.05)
+            return -1;
+        return 0;
     }
 };
 
